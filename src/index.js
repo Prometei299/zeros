b7564cfd3a0e5a8632e4e6251cdd3ade2c8d2736
Prometei299 <pavel-luchs@ya.ru>
2018-09-18 19:16:26 +0300

    ZEROOOOOOOOOS!!!!!!!!!!!!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+
+  let ZerosResult = 0;
+
+  while(number > 0){
+    number = Math.floor(number / 5);
+    ZerosResult = ZerosResult + number;
+  }
+  return ZerosResult;
 }
